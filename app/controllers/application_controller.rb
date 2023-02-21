aefaf76540200f52b45baea598807728c057d101
Kelvin-Ben <[kevoben14@gmail.com]>
2023-02-21 22:06:50 +0300

    Create authentication requirements

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery prepend: true
   before_action :update_allowed_parameters, if: :devise_controller?
-  before_action :authenticate_user!, except: [:splash]
+  before_action :authenticate_user!
+
+  def after_sign_in_path_for(_resource)
+    categories_path
+  end
 
   protected
 
