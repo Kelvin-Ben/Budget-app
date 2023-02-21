6b3f8bda2a2e37501cc8bb198be58fb858bec77a
Kelvin-Ben <[kevoben14@gmail.com]>
2023-02-21 22:07:59 +0300

    Modified model with associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
+
   has_many :categories, foreign_key: :author_id, dependent: :destroy
   has_many :expenses, foreign_key: :author_id, dependent: :destroy
 
